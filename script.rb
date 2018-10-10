2766cd0c3a548cbe7ffc04cac495bc8227e74157
Patryk Pilecki <ppilecki@icloud.com>
2018-10-10 23:02:32 +0100

    little refactor of find files method

--- script.rb
+++ script.rb
@@ -1,10 +1,11 @@
 files_list = Dir.children("sqlfiles")
 db_status = 46
+
 def newest_file_number(files_list)
   files_numbers = files_list.map { |file| file.gsub(/[^\d]/, '').to_i  }
   files_numbers.max
 end
+
 def find_files_to_exec(files_list, db_status)
-  files_to_exec = files_list.map { |file| file if file.gsub(/[^\d]/, '').to_i > db_status  }
-  files_to_exec.compact
+  files_to_exec = files_list.select { |file| file if file.gsub(/[^\d]/, '').to_i > db_status  }
 end
