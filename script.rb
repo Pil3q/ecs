cd565714d10a1c567022b120560f22b766cad030
Patryk Pilecki <ppilecki@icloud.com>
2018-10-10 22:56:57 +0100

    finding files to exec (nrs above the db status)

--- script.rb
+++ script.rb
@@ -1,6 +1,10 @@
 files_list = Dir.children("sqlfiles")
+db_status = 46
 def newest_file_number(files_list)
   files_numbers = files_list.map { |file| file.gsub(/[^\d]/, '').to_i  }
   files_numbers.max
 end
-p newest_file_number(files_list)
+def find_files_to_exec(files_list, db_status)
+  files_to_exec = files_list.map { |file| file if file.gsub(/[^\d]/, '').to_i > db_status  }
+  files_to_exec.compact
+end
--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -1,8 +1,15 @@
 require_relative "../script"
 
+test_files_names = ["045.somefile.sql", "022nodot.sql", "0002.moredigits.sql", "95.lessdigits.sql"]
+db_status = 44
+
 describe 'newest_file_number' do
   it 'finds the newest(highest) file number' do
-    test_files_names = ["045.somefile.sql", "022nodot.sql", "0002.moredigits.sql", "95.lessdigits.sql"]
     expect(newest_file_number(test_files_names)).to eq 95
+  end
+end
+describe 'find_files_to_exec' do
+  it 'finds the files that need to be executed' do
+    expect(find_files_to_exec(test_files_names, db_status)).to eq ["045.somefile.sql", "95.lessdigits.sql"]
   end
 end
